1988f09e4d0f37126f4fd947deddc73b36cfb4c3
rahe12 <144845749+rahe12@users.noreply.github.com>
2025-05-18 10:45:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ process.on('unhandledRejection', (err) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
